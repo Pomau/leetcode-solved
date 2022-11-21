0ea1474046815c5cff7149ced517fb197d61d06b
Timur <bikt38@gmail.com>
2022-11-21 16:24:10 +0300

    Time: 304 ms (64.19%), Space: 18.1 MB (85.72%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,24 +1,11 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         answer = []
-        main = set()
-        open = defaultdict(int)
-        close = defaultdict(int)
-        for interval in intervals:
-            open[interval[0]] += 1
-            close[interval[1]] += 1
-            main.add(interval[0])
-            main.add(interval[1])
-        events = sorted(list(main))
-        now = 0
-        for event in events:
-            if now == 0 and open[event] > 0:
-                answer.append([event])
-            now += open[event]
-            now -= close[event] 
-            if now == 0:
-                answer[-1].append(event)
+        for interval in sorted(intervals, key=lambda x: x[0]):
+            if len(answer) > 0 and answer[-1][1] >= interval[0]:
+                answer[-1][1] = max(answer[-1][1], interval[1])
+            else:
+                answer.append(interval)
         return answer
-            
              
         
