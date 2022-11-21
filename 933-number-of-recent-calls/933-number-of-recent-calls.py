5735cce798c7ed54cb01d72fc6b3031b5d059fc0
Timur <bikt38@gmail.com>
2022-11-21 12:32:28 +0300

    Time: 399 ms (77.75%), Space: 19.5 MB (76.58%) - LeetHub

--- 933-number-of-recent-calls/933-number-of-recent-calls.py
+++ 933-number-of-recent-calls/933-number-of-recent-calls.py
@@ -1,17 +1,41 @@
 class RecentCounter:
 
     def __init__(self):
-        self.arr = []
-        self.l = 0
+        self.q = Queue()
 
     def ping(self, t: int) -> int:
-        self.arr.append(t)
-        while t - self.arr[self.l] > 3000:
-            self.l += 1
-        return len(self.arr) - self.l
+        self.q.insert(t)
+        while t - self.q.back() > 3000:
+            self.q.remove()
+        return self.q.len()
+    
         
 
-
+class Queue:
+    def __init__(self):
+        self.arr_insert = []
+        self.arr_remove = []
+    
+    def insert(self, t):
+        self.arr_insert.append(t)
+    
+    def remove(self):
+        self.merge()
+        return self.arr_remove.pop()
+    
+    def back(self):
+        self.merge()
+        return self.arr_remove[-1]
+        
+    def merge(self):
+        if len(self.arr_remove) != 0:
+            return
+        while len(self.arr_insert) > 0:
+            back = self.arr_insert.pop()
+            self.arr_remove.append(back)
+    
+    def len(self):
+        return len(self.arr_insert) + len(self.arr_remove)
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
