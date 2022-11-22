a5c9a195217e61f691eaeb9ad1986bce40ce03d6
Timur <bikt38@gmail.com>
2022-11-22 17:12:23 +0300

    Time: 144 ms (71.10%), Space: 14 MB (32.05%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,16 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         alf = defaultdict(int)
-        for i in s1:
-            alf[i] += 1
-        for i in range(len(s2)):
-            alf[s2[i]] -= 1
-            if alf[s2[i]] == 0:
-                del alf[s2[i]]
-            if i >= len(s1):
-                alf[s2[i - len(s1)]] += 1
-                if alf[s2[i - len(s1)]] == 0:
-                    del alf[s2[i - len(s1)]]
+        for ch in s1:
+            alf[ch] += 1
+        for r in range(len(s2)):
+            alf[s2[r]] -= 1
+            if alf[s2[r]] == 0:
+                del alf[s2[r]]
+            if r >= len(s1):
+                alf[s2[r - len(s1)]] += 1
+                if alf[s2[r - len(s1)]] == 0:
+                    del alf[s2[r - len(s1)]]
             if len(alf) == 0:
                 return True
         return False
+            
