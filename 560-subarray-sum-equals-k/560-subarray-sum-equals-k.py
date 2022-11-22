c326da887852fc830939e9389340b67b9869636b
Timur <bikt38@gmail.com>
2022-11-22 17:33:06 +0300

    Time: 937 ms (7.77%), Space: 18.4 MB (8.68%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,11 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix_sums = defaultdict(int)
-        sums = 0
         answer = 0
-        prefix_sums[0] = 1
+        trains = defaultdict(int)
+        sums = 0
+        trains[0] = 1
         for i in range(len(nums)):
             sums += nums[i]
-            answer += prefix_sums[sums - k]
-            prefix_sums[sums] += 1
+            answer += trains[sums - k]
+            trains[sums] += 1
         return answer
+            
