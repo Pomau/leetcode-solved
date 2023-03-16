aabb5397d97e69bfeea989d07fc66ddff85f3f49
Timur <bikt38@gmail.com>
2023-03-16 13:53:56 +0300

    Time: 104 ms (30.60%), Space: 14.1 MB (52.86%) - LeetHub

---
+++ 748-shortest-completing-word/748-shortest-completing-word.py
@@ -0,0 +1,16 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        ans = "x" * 100
+        for word in words:
+            alf = defaultdict(int)
+            for ch in licensePlate:
+                if ch.isalpha():
+                    alf[ch.lower()] += 1
+            for ch in word:
+                if ch.lower() in alf:
+                    alf[ch.lower()] -= 1
+                    if alf[ch.lower()] == 0:
+                        del alf[ch.lower()]
+            if len(alf) == 0:
+                ans = min(ans, word, key=lambda x: len(x))
+        return ans
