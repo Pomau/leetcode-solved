52b4d69a94048426924ce076e7def5d37592cd4a
Timur <bikt38@gmail.com>
2023-03-18 14:39:49 +0300

    Time: 310 ms (75.33%), Space: 16.7 MB (65.78%) - LeetHub

---
+++ 470-implement-rand10-using-rand7/470-implement-rand10-using-rand7.py
@@ -0,0 +1,13 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        number = 41
+        while number > 40:
+            number = (rand7() - 1) * 7 + rand7()
+        return number % 10 + 1
