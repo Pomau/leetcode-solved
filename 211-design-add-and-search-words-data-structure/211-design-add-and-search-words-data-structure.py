380fe3ae3d41ca1b46f72485c6e6fc4de976bf82
Timur <bikt38@gmail.com>
2023-03-19 07:20:04 +0300

    Time: 7887 ms (83.97%), Space: 55.9 MB (90.76%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,38 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.head = {}
+
+    def addWord(self, word: str) -> None:
+        node = self.head
+        for ch in word:
+            if ch not in node:
+                node[ch] = {}
+            node = node[ch] 
+        node["$"] = True
+        
+
+    def search(self, word: str) -> bool:
+        return self._search(word, 0, self.head)
+    
+    def _search(self, word, l, node):
+        if l >= len(word):
+            print(word, l, node)
+            if "$" in node:
+                return True
+            return False
+        ans = False
+        if word[l] == ".":
+            for key, item in node.items():
+                if key == "$":
+                    continue
+                ans = ans or self._search(word, l + 1, item)
+        elif word[l] in node:
+            ans = self._search(word, l + 1, node[word[l]])
+        return ans
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
