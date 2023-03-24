0f2dcf4eb9d34e9b2b8f1a7dbcd5d2b4d7bc3b29
Timur <bikt38@gmail.com>
2023-03-24 22:17:53 +0300

    Time: 1458 ms (17.33%), Space: 40.7 MB (94.38%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        roads = [[[] for j in range(2)] for i in range(n)]
+        for road in connections:
+            roads[road[0]][1].append(road[1])
+            roads[road[1]][0].append(road[0])
+        ans = 0
+        q = [(0, -1)]
+        l = 0
+        while len(q) > l:
+            node, last = q[l]
+            l += 1
+            print(node, last)
+            for road in roads[node][0]:
+                if road == last:
+                    continue
+                q.append((road, node))
+            for road in roads[node][1]:
+                if road == last:
+                    continue
+                q.append((road, node))
+                ans += 1
+        return ans
