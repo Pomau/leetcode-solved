7e6373e8697a7483dc3309721e8d92791d14b4af
Timur <bikt38@gmail.com>
2023-04-07 01:56:37 +0300

    Time: 141 ms (30.39%), Space: 14.4 MB (61.58%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -6,7 +6,6 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 0 and self.color[i][j] == -1 and self.bfs(i, j, 1):
-                    print(i, j)
                     ans += 1
         return ans
     def bfs(self, i, j, color):
